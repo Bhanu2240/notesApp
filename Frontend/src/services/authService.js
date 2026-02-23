2b141dcc9afe07cb539d33ef72567f83e7da7cb8
Bhanu2240 <152272709+Bhanu2240@users.noreply.github.com>
2026-02-23 16:02:08 +0530

    Update API URL to production endpoint

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://notesapp-backend-bkgn.onrender.com/api/auth";
 
 export const signupApi = async (email, password) => {
   const response = await fetch(`${API_URL}/signup`, {
